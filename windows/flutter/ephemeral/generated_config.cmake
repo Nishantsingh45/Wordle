efe26f38ad98f905ede236ec810f21847bacce42
Nishantsingh45 <68099982+Nishantsingh45@users.noreply.github.com>
2022-05-21 16:42:41 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\nisha\\Desktop\\Flutter\\game\\wordle" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\nisha\\Desktop\\Flutter\\game\\wordle"
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\nisha\\Desktop\\Flutter\\game\\wordle\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\nisha\\Desktop\\Flutter\\game\\wordle"
+  "FLUTTER_TARGET=C:\\Users\\nisha\\Desktop\\Flutter\\game\\wordle\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\nisha\\Desktop\\Flutter\\game\\wordle\\.dart_tool\\package_config.json"
+)
